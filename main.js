4cbc196b583eb957a5893cb65e12b609455d3a8d
Jyotsna265 <74059430+Jyotsna265@users.noreply.github.com>
2021-08-16 18:25:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,14 +8,42 @@ function setup() {
 	canvas = createCanvas(1240, 336);
 	canvas.parent('canvas');
 	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+	video.size(800, 300);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.poseNet('pose', gotPoses);
 }
 
 function draw() {
 	game()
 }
 
+function modelLoaded() {
+	console.log('Model Loaded!!!');
+}
 
+function gotPoses(results) {
+	if (results.length > 0) {
+		console.log(results);
+		noseX = results[0].poseNet.noseX.noseX;
+		noseX = results[0].poseNet.noseX.noseX;
+	}
+}
 
+noseX = "";
+noseY = "";
+GameStatus = "";
 
+function startGame() {
+	GameStatus = "start";
+	document.getElementById("status").innerHTML = "Game Is Loading";
+}
 
-
+	function game() {
+		instializeInDraw();
+		moveEnvironment(mario);
+		drawSprites();
+	}
